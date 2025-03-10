28b31009c0280baf28ad035a2ccb2fc10b310474
AbHaMaDa <abdallahhamada2103@gmail.com>
2025-03-11 00:52:04 +0200

    adding pump in manual mode

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,20 +1,18 @@
 #include <LiquidCrystal_I2C.h>  // Include the LiquidCrystal_I2C library
+#include <Servo.h>  // Include the Servo library
 
 /* Create an instance of the LCD display */
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// defines varibles  for line_following 
-#define NUM_SENSORS 5  
-#define MAX_SPEED 200
-
-
-#define MOTOR_L_IN1       13
+#define MOTOR_L_IN1       12
 #define MOTOR_L_IN2       11
 #define MOTOR_R_IN3       8
 #define MOTOR_R_IN4       7 
 #define speedControlerR   10 
 #define speedControlerL   9
 
+#define relayPin  13 // Connect relay module to digital pin 13
+#define SERVO_PIN 6  // Define the pin for the servo motor
 
 
 #define sensor_1st A0
@@ -24,14 +22,14 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 #define sensor_5th A4
 
 
+Servo fireServo; // Create a servo object
+
+
+
 // variables that are used in the code
-int speedValue = 255 ; // for manual mode
+int speedValue = 255 ;
 char mode = 'M';   // M || L || F
-char order= 'k';
-
-
-
-
+char order ='k';
 
 // fun for lcd
 void lcdSpeed();
@@ -52,8 +50,8 @@ void movingBackwardLeft();
 void movingBackwardRight();
 void speedUp();
 void speedDown();
-void stopAllFuns();
-void moveMotors(int leftSpeed, int rightSpeed);
+void activateFireExtinguisher();
+void stopFireExtinguisher();
 
 
 
@@ -66,13 +64,18 @@ void setup() {
   pinMode(MOTOR_R_IN4, OUTPUT);
   pinMode(speedControlerR, OUTPUT);
   pinMode(speedControlerL, OUTPUT);
-
+  //sensors pins
   pinMode(sensor_1st,INPUT);
   pinMode(sensor_2nd,INPUT);
   pinMode(sensor_3th,INPUT);
   pinMode(sensor_4th,INPUT);
   pinMode(sensor_5th,INPUT);
 
+  pinMode(relayPin, OUTPUT); // Set the relay pin as an output
+  digitalWrite(relayPin, LOW); // Ensure the relay is off at startup
+
+  fireServo.attach(SERVO_PIN); // Attach servo to pin 6
+  fireServo.write(90); // Set servo to start position
 
 
   Serial.begin(9600); // Use the default Serial
@@ -90,37 +93,33 @@ void setup() {
 }
 
 void loop() {
-  // check if the serial port is available
-    if (Serial.available()) {
-      char input = Serial.read(); // read the incoming byte:
-      Serial.print("Received: ");
-      Serial.println(input);
+ // Step 1: Check if a new command is available
+  if (Serial.available()) {
+    char input = Serial.read();
+    Serial.print("Received: ");
+    Serial.println(input);
 
-      if(input == 'M' || input == 'L' || input == 'F'){ // check if the input is one of the modes
-          stopAllFuns();
-          mode = input;
-          lcd.clear();  // Clear the previous message
-          switch (mode){
-          case 'M':lcd.print("You Now Are In Manual Mode");break;
-          case 'L':lcd.print("You Now Are In Line Following Mode");break;
-          case 'F':lcd.print("You Now Are In Fire Fighting Mode");break;
-          }
-      }else{
-        order = input;
-        Serial.print("Received: ");
-        Serial.println(order);
+    if (input == 'M' || input == 'L' || input == 'F') {
+      mode = input;  // Update the mode immediately
+      lcd.clear();
+      switch (mode) {
+        case 'M': lcd.print("Manual Mode"); Serial.println("Mode: Manual"); break;
+        case 'L': lcd.print("Line Following Mode"); Serial.println("Mode: Line Following"); break;
+        case 'F': lcd.print("Fire Fighting Mode"); Serial.println("Mode: Fire Fighting"); break;
       }
+    } else {
+      order = input;  // Store order if it's not a mode change
+    }
   }
 
-  switch (mode){
-    case 'M':Manual();break;
-    case 'L':LineFollowing();break;
-    case 'F':FireFighting();break;
+  // Step 2: Run the correct mode function (Each function runs only once per cycle)
+  if (mode == 'M') {
+    Manual();
+  } else if (mode == 'L') {
+    LineFollowing();
+  } else if (mode == 'F') {
+    FireFighting();
   }
-
-
-  
-
 
 
 }
@@ -131,7 +130,6 @@ void loop() {
 
 
 void Manual(){ //1st mode
-    if (mode != 'M') return;  // Exit if mode changed
 
   // set the speed value to be between 0 and 255
   speedValue = constrain(speedValue, 0, 255);
@@ -148,31 +146,35 @@ void Manual(){ //1st mode
     case 'C':movingBackwardRight();break;
     case 'U':speedUp();break;
     case 'X':speedDown();break;
+    case 'R':activateFireExtinguisher();break;
+    case 'O':stopFireExtinguisher();break;
+    default: stopMoving(); break;  // <-- ADD THIS TO STOP WHEN NO VALID COMMAND
+
   }
   order = ' ';
 }
 
 
 void LineFollowing(){ //2nd mode
-    if (mode != 'L') return;  // Exit if mode changed
-    speedValue = 180;
-    // code of line following
-    if((digitalRead(sensor_2nd) == 0)  && (digitalRead(sensor_4th) == 0)){
-      movingForward();
-    }else if ((digitalRead(sensor_2nd) == 1)  && (digitalRead(sensor_4th) == 0)){
-      turningLeft();
-    }else if ((digitalRead(sensor_2nd) == 0)  && (digitalRead(sensor_4th) == 1)){
-      turningRight();
-    }else if ((digitalRead(sensor_2nd) == 1)  && (digitalRead(sensor_4th) == 1)){
-      stopMoving();
-    }
+  if (mode != 'L') return;  // Stop immediately if mode has changed
 
+  speedValue = 180;
+  // code of line following
+  if((digitalRead(sensor_2nd) == 0)  && (digitalRead(sensor_4th) == 0)){
+    movingForward();
+  }else if ((digitalRead(sensor_2nd) == 1)  && (digitalRead(sensor_4th) == 0)){
+    turningLeft();
+  }else if ((digitalRead(sensor_2nd) == 0)  && (digitalRead(sensor_4th) == 1)){
+    turningRight();
+  }else if ((digitalRead(sensor_2nd) == 1)  && (digitalRead(sensor_4th) == 1)){
+    stopMoving();
+  }
+  if (mode != 'L') return;  // Check again before taking action
+
+  
 }
 
-void FireFighting(){ //3th
-  if (mode != 'F') return;  // Exit if mode changed
-
-// code of fire fighting
+void FireFighting() { 
 
 }
 
@@ -239,7 +241,7 @@ void movingForwardLeft(){
   digitalWrite(MOTOR_R_IN3, HIGH);
   digitalWrite(MOTOR_R_IN4, LOW);
   analogWrite(speedControlerR, speedValue);
-  analogWrite(speedControlerL, speedValue-40);
+  analogWrite(speedControlerL, speedValue-120);
   lcdMessage("Moving Forward Left");
 
 }
@@ -248,7 +250,7 @@ void movingForwardRight(){
   digitalWrite(MOTOR_L_IN2, LOW);
   digitalWrite(MOTOR_R_IN3, HIGH);
   digitalWrite(MOTOR_R_IN4, LOW);
-  analogWrite(speedControlerR, speedValue-40);
+  analogWrite(speedControlerR, speedValue-120);
   analogWrite(speedControlerL, speedValue);
   lcdMessage("Moving Forward Right");
 
@@ -259,7 +261,7 @@ void movingBackwardLeft(){
   digitalWrite(MOTOR_R_IN3, LOW);
   digitalWrite(MOTOR_R_IN4, HIGH);
   analogWrite(speedControlerR, speedValue);
-  analogWrite(speedControlerL, speedValue-40);
+  analogWrite(speedControlerL, speedValue-120);
   lcdMessage("Moving Backward Left");
 
 }
@@ -269,7 +271,7 @@ void movingBackwardRight(){
   digitalWrite(MOTOR_L_IN2, HIGH);
   digitalWrite(MOTOR_R_IN3, LOW);
   digitalWrite(MOTOR_R_IN4, HIGH);
-  analogWrite(speedControlerR, speedValue-40);
+  analogWrite(speedControlerR, speedValue-120);
   analogWrite(speedControlerL, speedValue);
   lcdMessage("Moving Backward Right");
 
@@ -310,31 +312,42 @@ void lcdMessage(const char* message) {
 
 
 
+void activateFireExtinguisher() {
+    static bool pumpState = false;
+    static int servoAngle = 90;
+    static bool movingForward = true;
+    static unsigned long lastServoMove = 0;
 
-void stopAllFuns(){
-    Serial.println("Stopping previous mode...");
+    if (!pumpState) {
+        digitalWrite(relayPin, HIGH);  // Turn pump ON
+        Serial.println("Fire Extinguisher ON");
+        pumpState = true;
+    }
+
+    // Move the servo back and forth
+    if (millis() - lastServoMove >= 140) {  // Move every 140ms
+        if (movingForward) {
+            servoAngle += 10;
+            if (servoAngle >= 180) movingForward = false;
+        } else {
+            servoAngle -= 10;
+            if (servoAngle <= 0) movingForward = true;
+        }
+        fireServo.write(servoAngle);
+        lastServoMove = millis();
+    }
+}
+
+void stopFireExtinguisher() {
+    digitalWrite(relayPin, LOW);  // Turn pump OFF
+    Serial.println("Fire Extinguisher OFF");
+    fireServo.write(90);  // Reset servo position
 }
 
 
 
-// void moveMotors(int leftSpeed, int rightSpeed) {
-//     // Left Motor Control
-//     if (leftSpeed > 0) {
-//         digitalWrite(MOTOR_L_IN1, HIGH);
-//         digitalWrite(MOTOR_L_IN2, LOW);
-//     } else {
-//         digitalWrite(MOTOR_L_IN1, LOW);
-//         digitalWrite(MOTOR_L_IN2, HIGH);
-//     }
-//     analogWrite(speedControlerL, abs(leftSpeed));
 
-//     // Right Motor Control
-//     if (rightSpeed > 0) {
-//         digitalWrite(MOTOR_R_IN3, HIGH);
-//         digitalWrite(MOTOR_R_IN4, LOW);
-//     } else {
-//         digitalWrite(MOTOR_R_IN3, LOW);
-//         digitalWrite(MOTOR_R_IN4, HIGH);
-//     }
-//     analogWrite(speedControlerR, abs(rightSpeed));
-// }
+
+
+
+
